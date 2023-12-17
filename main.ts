2d836dd12098a972e61078fa366c476fbbcbc258
Dylan Howell <dylanleehowell@gmail.com>
2023-12-16 23:59:18 -0600

    Add useless bricks

--- main.ts
+++ main.ts
@@ -10,77 +10,114 @@ const paddleWidth = 75;
 let paddleX = (canvas.width - paddleWidth) / 2;
 let rightPressed = false;
 let leftPressed = false;
+const brickRowCount = 3;
+const brickColumnCount = 5;
+const brickWidth = 75;
+const brickHeight = 20;
+const brickPadding = 10;
+const brickOffsetTop = 30;
+const brickOffsetLeft = 30;
+const bricks: Point[][] = [];
+
+interface Point {
+  x: number;
+  y: number;
+}
+
+for (let c = 0; c < brickColumnCount; c++) {
+  bricks[c] = [];
+  for (let r = 0; r < brickRowCount; r++) {
+    bricks[c][r] = { x: 0, y: 0 };
+  }
+}
 
 document.addEventListener('keydown', keyDownHandler, false);
 document.addEventListener('keyup', keyUpHandler, false);
 
 function keyDownHandler(e: KeyboardEvent) {
-    if (e.key == 'Right' || e.key == 'ArrowRight') {
-        rightPressed = true;
-    } else if (e.key == 'Left' || e.key == 'ArrowLeft') {
-        leftPressed = true;
-    }
+  if (e.key == 'Right' || e.key == 'ArrowRight') {
+    rightPressed = true;
+  } else if (e.key == 'Left' || e.key == 'ArrowLeft') {
+    leftPressed = true;
+  }
 }
 
 function keyUpHandler(e: KeyboardEvent) {
-    if (e.key == 'Right' || e.key == 'ArrowRight') {
-        rightPressed = false;
-    } else if (e.key == 'Left' || e.key == 'ArrowLeft') {
-        leftPressed = false;
-    }
+  if (e.key == 'Right' || e.key == 'ArrowRight') {
+    rightPressed = false;
+  } else if (e.key == 'Left' || e.key == 'ArrowLeft') {
+    leftPressed = false;
+  }
 }
 
 function drawBall() {
-    ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-    ctx.fillStyle = '#0095DD';
-    ctx.fill();
-    ctx.closePath();
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = '#0095DD';
+  ctx.fill();
+  ctx.closePath();
 }
 function drawPaddle() {
-    ctx.beginPath();
-    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = '#0095DD';
-    ctx.fill();
-    ctx.closePath();
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = '#0095DD';
+  ctx.fill();
+  ctx.closePath();
 }
 
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-    drawPaddle();
-    handleXWallCollision();
-    handleYWallCollision();
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  drawPaddle();
+  drawBricks();
+  handleXWallCollision();
+  handleYWallCollision();
 
-    if (rightPressed && paddleX < canvas.width - paddleWidth) {
-        paddleX += 7;
-    } else if (leftPressed && paddleX > 0) {
-        paddleX -= 7;
-    }
+  if (rightPressed && paddleX < canvas.width - paddleWidth) {
+    paddleX += 7;
+  } else if (leftPressed && paddleX > 0) {
+    paddleX -= 7;
+  }
 
-    x += dx;
-    y += dy;
+  x += dx;
+  y += dy;
 }
 
 function handleXWallCollision() {
-    // if ball is colliding with left or right wall.
-    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
-        dx *= -1;
-    }
+  // if ball is colliding with left or right wall.
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx *= -1;
+  }
 }
 
 function handleYWallCollision() {
-    if (shouldBounceOffTop()) {
-        dy *= -1;
-    } else if (ballIsCollidingWithBottom()) {
-        if (ballIsAbovePaddle()) {
-            dy *= -1;
-        } else {
-            alert('GAME OVER');
-            document.location.reload();
-            clearInterval(interval); // Needed for Chrome to end game
-        }
+  if (shouldBounceOffTop()) {
+    dy *= -1;
+  } else if (ballIsCollidingWithBottom()) {
+    if (ballIsAbovePaddle()) {
+      dy *= -1;
+    } else {
+      alert('GAME OVER');
+      document.location.reload();
+      clearInterval(interval); // Needed for Chrome to end game
     }
+  }
+}
+
+function drawBricks() {
+  for (let c = 0; c < brickColumnCount; c++) {
+    for (let r = 0; r < brickRowCount; r++) {
+      const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+      const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+      bricks[c][r].x = brickX;
+      bricks[c][r].y = brickY;
+      ctx.beginPath();
+      ctx.rect(brickX, brickY, brickWidth, brickHeight);
+      ctx.fillStyle = '#0095DD';
+      ctx.fill();
+      ctx.closePath();
+    }
+  }
 }
 
 const ballIsCollidingWithBottom = () => y + dy > canvas.height - ballRadius;
